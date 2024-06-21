024a0a4d4ffb92d065ed198c5ded9260769dd18c
kalia <fmoon11200@naver.com>
2024-06-21 10:28:47 +0900

    feat: hostroom 에러 핸들러 코드 추가

---
+++ airbnb/src/main/java/team8/airbnb/exception/ErrorResponse.java
@@ -0,0 +1,14 @@
+package team8.airbnb.exception;
+
+import lombok.Getter;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+  private final String message;
+
+  public ErrorResponse(String message) {
+    this.message = message;
+  }
+}
